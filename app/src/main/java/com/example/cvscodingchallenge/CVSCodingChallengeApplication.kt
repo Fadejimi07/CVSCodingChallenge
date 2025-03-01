9b420f4ce455dbcd4329205aa47d8e569dae68d1
Fadejimi <adegbulugbefadejimi@gmail.com>
2025-02-28 23:24:00 -0600

    Add the network dependency integration

---
+++ app/src/main/java/com/example/cvscodingchallenge/CVSCodingChallengeApplication.kt
@@ -0,0 +1,7 @@
+package com.example.cvscodingchallenge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CVSCodingChallengeApplication : Application()
